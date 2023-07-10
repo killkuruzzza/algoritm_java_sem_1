dc2ab26dc80dd0e49fa5f2ac64fd110e6fb8be8d
Moiseev Kirill <kilkuruzzza@mail.ru>
2023-07-10 21:56:32 +0300

    update

--- src/task1.java
+++ src/task1.java
@@ -1,2 +1,13 @@
 public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int sum(int lastNumber) {
+        int sum = 0;
+        for (int i = 1; i <=lastNumber; i++){
+            sum+=i;
+        }
+        return sum;
+    }
 }
